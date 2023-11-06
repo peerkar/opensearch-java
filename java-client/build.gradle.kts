11609518361934dce31426e764d85de5a88216aa
Petteri Karttunen <petteri.karttunen@liferay.com>
2023-11-06 06:24:26 +0200

    Set Java 8 compatibility

--- java-client/build.gradle.kts
+++ java-client/build.gradle.kts
@@ -61,8 +61,8 @@ configurations {
 }
 
 java {
-    targetCompatibility = JavaVersion.VERSION_11
-    sourceCompatibility = JavaVersion.VERSION_11
+    targetCompatibility = JavaVersion.VERSION_1_8
+    sourceCompatibility = JavaVersion.VERSION_1_8
 
     withJavadocJar()
     withSourcesJar()
